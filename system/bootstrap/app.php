e909913de02672297c57f2dcb75fdecc9494363a
itel23id-boop <itel23.id@gmail.com>
2025-08-27 21:17:35 +0700

    refactor: remove route service provider

--- system/bootstrap/app.php
+++ system/bootstrap/app.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        console: __DIR__.'/../routes/console.php',
     )
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command('get:notify')->everyMinute();
@@ -24,4 +27,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('update:pesanan')->everyMinute();
     })
     ->create();
+
+RateLimiter::for('api', fn (Request $r) =>
+    Limit::perMinute(60)->by($r->user()?->id ?? $r->ip()));
+
+return $app;
 

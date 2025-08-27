f476241add326014e95074217918d45d1b05be8f
itel23id-boop <itel23.id@gmail.com>
2025-08-27 21:31:24 +0700

    refactor: inline routing config

--- system/bootstrap/app.php
+++ system/bootstrap/app.php
@@ -3,13 +3,16 @@
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        console: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(
         middleware: [
@@ -78,3 +81,8 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->create();
 
+RateLimiter::for('api', function (Request $request) {
+    return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+});
+
+return $app;

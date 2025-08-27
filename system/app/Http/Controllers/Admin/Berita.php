34a2ad3d28434657d1b2496d49a30fc5ea5f2972
itel23id-boop <itel23.id@gmail.com>
2025-08-27 21:45:56 +0700

    Use storage facade for banner uploads

--- system/app/Http/Controllers/Admin/Berita.php
+++ system/app/Http/Controllers/Admin/Berita.php
@@ -8,6 +8,8 @@ use App\Models\Berita as BeritaModel;
 use Illuminate\Support\Facades\Auth;
 use App\Models\LogUser;
 use App\Helpers\Formater;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class Berita extends Controller
 {
@@ -29,11 +31,11 @@ class Berita extends Controller
         ]);
 
         $file = $request->file('banner');
-        $folder = 'assets/banner';
-        $file->move($folder, $file->getClientOriginalName());
-        
+        $filename = Str::uuid()->toString().'.'.$file->getClientOriginalExtension();
+        $storedPath = $file->storeAs('banners', $filename, 'public');
+
         $berita = new BeritaModel();
-        $berita->path = "/".$folder."/".$file->getClientOriginalName();
+        $berita->path = Storage::url($storedPath);
         $berita->deskripsi = isset($request->deskripsi) ? $request->deskripsi : null;
         $berita->tipe = $request->tipe;
         $berita->save();
@@ -56,8 +58,13 @@ class Berita extends Controller
         $client_ip = $formatter->client_ip();
         $client_iploc = $formatter->client_iploc($client_ip);
         $browser = $formatter->devices();
-        
-        BeritaModel::where('id', $id)->delete();
+
+        $berita = BeritaModel::findOrFail($id);
+        if ($berita->path) {
+            $relativePath = str_replace('/storage/', '', $berita->path);
+            Storage::disk('public')->delete($relativePath);
+        }
+        $berita->delete();
         
         $log = new LogUser();
         $log->user = Auth::user()->username;
